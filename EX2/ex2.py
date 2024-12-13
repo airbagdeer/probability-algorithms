41534bdc18b25c4aa5b9c7889792345773a6e66b
eyal stolov <eystolov@gmail.com>
2024-12-13 17:06:25 +0200

    finished part 1

--- EX2/ex2.py
+++ EX2/ex2.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 from file_handling_service import File_handler_service
 
 #TODO: in file handler service change the delimiter to tab not new line
@@ -17,30 +18,101 @@ V = 300000
 file_handling_service = File_handler_service(args.output_file_name)
 file_handling_service.create_output_file()
 
-def q1():
+def lidstone_smoothing(c_x, _lambda, S_size, X_size):
+    return (c_x + _lambda) / (S_size + _lambda * X_size)
+
+def perplexity_measure(s_validation_set, training_words_probabilities, p_unseen_word):
+    perplexity = 0
+
+    for word in s_validation_set:
+        if word in training_words_probabilities:
+            perplexity = perplexity + math.log(training_words_probabilities[word])
+        else:
+            perplexity = perplexity + math.log(p_unseen_word)
+
+    perplexity = math.pow(2, (-1/len(s_validation_set))*perplexity)
+
+    return perplexity
+
+def lidstone_smoothing_model(training_set, training_events, _lambda):
+    training_words_probabilities = {}
+
+    for word in training_set:
+        training_words_probabilities[word] = lidstone_smoothing(training_events[word], _lambda, len(training_set), len(training_events))
+
+    return training_words_probabilities
+
+def main():
     file_handling_service.write_output(1, args.train_file_name)
     file_handling_service.write_output(2, args.test_file_name)
     file_handling_service.write_output(3, args.input_word)
     file_handling_service.write_output(4, args.output_file_name)
     file_handling_service.write_output(5, V)
+    file_handling_service.write_output(6, 1/V)
 
-    train_file_words = file_handling_service.get_only_words_in_train_file(args.train_file_name)
-    words_unique_values = {}
+    S = file_handling_service.get_only_words_in_file(args.train_file_name)
+    S_size = len(S)
+    file_handling_service.write_output(7, S_size)
 
-    for word in train_file_words:
-        if word in words_unique_values:
-            words_unique_values[word] +=1
+    training_set_size = round(0.9 * S_size)
+    s_validation_set = S[training_set_size:]
+    s_training_set = S[:training_set_size]
+    s_training_set_size = len(s_training_set)
+
+    file_handling_service.write_output(8, len(s_validation_set))
+    file_handling_service.write_output(9, len(s_training_set))
+
+    training_words_unique_values = {}
+
+    for word in s_training_set:
+        if word in training_words_unique_values:
+            training_words_unique_values[word] +=1
         else:
-            words_unique_values[word] = 1
+            training_words_unique_values[word] = 1
 
+    file_handling_service.write_output(10, len(training_words_unique_values.keys()))
 
-    if(args.input_word in words_unique_values):
-        p_input_word = words_unique_values[args.input_word]/len(train_file_words)
-        file_handling_service.write_output(6, p_input_word)
+    if(args.input_word in training_words_unique_values):
+        c_input_word = training_words_unique_values[args.input_word]
     else:
-        file_handling_service.write_output(6, 0)
+        c_input_word = 0
 
-    #TODO: not sure if this is what was intended:
-    file_handling_service.write_output(7, len(words_unique_values.keys()))
+    file_handling_service.write_output(11, c_input_word)
 
-q1()
+    p_input_words_mle = c_input_word / len(s_training_set)
+    file_handling_service.write_output(12, p_input_words_mle)
+
+    p_unseen_word = 0 / len(s_training_set) #can write immidiatly 0
+    file_handling_service.write_output(13, p_unseen_word)
+
+    _lambda = 0.10
+    training_words_unique_values_size = len(training_words_unique_values.keys())
+    p_lid_input_word = lidstone_smoothing(c_input_word, _lambda, s_training_set_size,training_words_unique_values_size)
+
+    file_handling_service.write_output(14, p_lid_input_word)
+
+    c_unseen_word = 0
+    p_lid_unseen_word = lidstone_smoothing(c_unseen_word, _lambda, s_training_set_size, training_words_unique_values_size)
+    file_handling_service.write_output(15, p_lid_unseen_word)
+
+    _lambdas = [0.01, 0.10, 1.00]
+
+    for index, _lambda in enumerate(_lambdas):
+        model = lidstone_smoothing_model(s_training_set, training_words_unique_values, _lambda)
+        p_lid_unseen_word = lidstone_smoothing(c_unseen_word, _lambda, s_training_set_size, training_words_unique_values_size)
+        perplexity = perplexity_measure(s_validation_set, model, p_lid_unseen_word)
+        file_handling_service.write_output(16+index, perplexity)
+
+    all_possible_lambdas = [i / 100 for i in range(1, 201)]
+    perplexities = []
+
+    for index, _lambda in enumerate(all_possible_lambdas):
+        model = lidstone_smoothing_model(s_training_set, training_words_unique_values, _lambda)
+        p_lid_unseen_word = lidstone_smoothing(c_unseen_word, _lambda, s_training_set_size, training_words_unique_values_size)
+        perplexity = perplexity_measure(s_validation_set, model, p_lid_unseen_word)
+        perplexities.append((perplexity, _lambda))
+
+    min_perplexity = min(perplexities, key=lambda x: x[0])
+    file_handling_service.write_output(19, min_perplexity[1])
+    file_handling_service.write_output(20, min_perplexity[0])
+main()
--- EX2/file_handling_service.py
+++ EX2/file_handling_service.py
@@ -1,29 +1,31 @@
 class File_handler_service:
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
     def __init__(self, output_file_name):
         self.output_file_name = output_file_name
 
     def create_output_file(self):
-        with open(self.output_file_name, "w") as file:
-            file.write("#Students Eyal Stolov Michael Chernin 324827328 314950668\n")
+        with open(self.output_file_name, "w", encoding="utf-8") as file:
+            file.write('#Students\tEyal Stolov\tMichael Chernin\t324827328\t314950668\n')
 
 
     def write_output(self, output_number, output_text):
         with open(self.output_file_name, "a") as file:
-            file.write(f'#Output{output_number} {output_text}\n')
+            file.write(f'#Output{output_number}:\t{output_text}\n')
 
-    def get_only_words_in_train_file(self, train_file_name):
+    def get_only_words_in_file(self, train_file_name):
         words_in_file = []
         with open(train_file_name, "r") as file:
             for line in file:
-                words_split_by_space = line.split(' ')
+                words_split_by_space = line.split()
                 for word in words_split_by_space:
-                    if word.isalpha():
-                        words_in_file.append(word)
-                    else:
-                        #TODO: what do we do with the words here
-                        print(word)
+                    words_in_file.append(word)
+                    #TODO: ask if words like punctuations are ok and if in topics the words with < is ok meanning '<TRAIN'
 
+                    # if '\n' in word:
+                    #     if word != '\n':
+                    #         words_in_file.append(word.replace('\n', ''))
+                    #     else:
+                    #         words_split_by_space.remove(word)
+                    # else:
+                    #     words_in_file.append(word)
 
             return words_in_file
